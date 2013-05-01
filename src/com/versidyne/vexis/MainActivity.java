b1620b8445e2667e334a80337ddb555b411a8b7a
Alex Gurrola <alex.gurrola@versidyne.com>
2013-05-01 01:29:36 -0700

    Updated Menu List

--- src/com/versidyne/vexis/MainActivity.java
+++ src/com/versidyne/vexis/MainActivity.java
@@ -81,8 +81,8 @@ public class MainActivity extends FragmentActivity {
 
 		@Override
 		public int getCount() {
-			// Show 3 total pages.
-			return 3;
+			// Show 9 total pages.
+			return 9;
 		}
 
 		@Override
