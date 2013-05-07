30c9879887cb6f5800d63ed9cd4a322712b022e8
Alex Gurrola <alex.gurrola@versidyne.com>
2013-05-06 22:59:22 -0700

    Compiled Source Files

--- gen/com/versidyne/vexis/R.java
+++ gen/com/versidyne/vexis/R.java
@@ -8,54 +8,122 @@
 package com.versidyne.vexis;
 
 public final class R {
+    public static final class array {
+        public static final int pref_example_list_titles=0x7f060000;
+        public static final int pref_example_list_values=0x7f060001;
+        public static final int pref_sync_frequency_titles=0x7f060002;
+        public static final int pref_sync_frequency_values=0x7f060003;
+    }
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int example_picture=0x7f020000;
+        public static final int ic_action_stat_reply=0x7f020001;
+        public static final int ic_action_stat_share=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int ic_stat_message=0x7f020004;
     }
     public static final class id {
-        public static final int TextView01=0x7f060003;
-        public static final int TextView02=0x7f060004;
-        public static final int TextView03=0x7f060006;
-        public static final int TextView04=0x7f060007;
-        public static final int TextView05=0x7f060009;
-        public static final int TextView06=0x7f06000a;
-        public static final int TextView07=0x7f06000d;
-        public static final int TextView08=0x7f06000e;
-        public static final int TextView09=0x7f060010;
-        public static final int TextView10=0x7f060011;
-        public static final int TextView11=0x7f060013;
-        public static final int TextView12=0x7f060014;
-        public static final int item_detail=0x7f06000b;
-        public static final int item_detail_container=0x7f060000;
-        public static final int item_list=0x7f060001;
-        public static final int linearLayout1=0x7f060002;
-        public static final int linearLayout2=0x7f060005;
-        public static final int linearLayout3=0x7f060008;
-        public static final int linearLayout4=0x7f06000c;
-        public static final int linearLayout5=0x7f06000f;
-        public static final int linearLayout6=0x7f060012;
-        public static final int login_button=0x7f060019;
-        public static final int textView1=0x7f060017;
-        public static final int textView2=0x7f060015;
-        public static final int txt_email=0x7f060016;
-        public static final int txt_password=0x7f060018;
+        public static final int TextView01=0x7f09000a;
+        public static final int TextView02=0x7f09000b;
+        public static final int TextView03=0x7f09000d;
+        public static final int TextView04=0x7f09000e;
+        public static final int TextView05=0x7f090010;
+        public static final int TextView06=0x7f090011;
+        public static final int TextView07=0x7f090014;
+        public static final int TextView08=0x7f090015;
+        public static final int TextView09=0x7f090017;
+        public static final int TextView10=0x7f090018;
+        public static final int TextView11=0x7f09001a;
+        public static final int TextView12=0x7f09001b;
+        public static final int action_forgot_password=0x7f090021;
+        public static final int email=0x7f090005;
+        public static final int item_detail=0x7f090012;
+        public static final int item_detail_container=0x7f090000;
+        public static final int item_list=0x7f090001;
+        public static final int linearLayout1=0x7f090009;
+        public static final int linearLayout2=0x7f09000c;
+        public static final int linearLayout3=0x7f09000f;
+        public static final int linearLayout4=0x7f090013;
+        public static final int linearLayout5=0x7f090016;
+        public static final int linearLayout6=0x7f090019;
+        public static final int login=0x7f090007;
+        public static final int login_button=0x7f090020;
+        public static final int login_form=0x7f090004;
+        public static final int login_status=0x7f090002;
+        public static final int login_status_message=0x7f090003;
+        public static final int password=0x7f090006;
+        public static final int sign_in_button=0x7f090008;
+        public static final int textView1=0x7f09001e;
+        public static final int textView2=0x7f09001c;
+        public static final int txt_email=0x7f09001d;
+        public static final int txt_password=0x7f09001f;
     }
     public static final class layout {
         public static final int activity_item_detail=0x7f030000;
         public static final int activity_item_list=0x7f030001;
         public static final int activity_item_twopane=0x7f030002;
-        public static final int device=0x7f030003;
-        public static final int fragment_item_detail=0x7f030004;
-        public static final int location=0x7f030005;
-        public static final int login=0x7f030006;
+        public static final int activity_login=0x7f030003;
+        public static final int device=0x7f030004;
+        public static final int fragment_item_detail=0x7f030005;
+        public static final int location=0x7f030006;
+        public static final int login=0x7f030007;
+    }
+    public static final class menu {
+        public static final int login=0x7f080000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int title_item_detail=0x7f040001;
-        public static final int title_item_list=0x7f040002;
+        public static final int action_forgot_password=0x7f05000c;
+        public static final int action_reply=0x7f050006;
+        public static final int action_share=0x7f050005;
+        public static final int action_sign_in_register=0x7f05000a;
+        public static final int action_sign_in_short=0x7f05000b;
+        public static final int app_name=0x7f050000;
+        public static final int error_field_required=0x7f050011;
+        public static final int error_incorrect_password=0x7f050010;
+        public static final int error_invalid_email=0x7f05000e;
+        public static final int error_invalid_password=0x7f05000f;
+        public static final int login_progress_signing_in=0x7f05000d;
+        public static final int message_notification_placeholder_text_template=0x7f050004;
+        public static final int message_notification_title_template=0x7f050003;
+        public static final int pref_default_display_name=0x7f050017;
+        public static final int pref_description_social_recommendations=0x7f050015;
+        /**  Example settings for Data & Sync 
+         */
+        public static final int pref_header_data_sync=0x7f050019;
+        /**  Strings related to Settings 
+ Example General settings 
+         */
+        public static final int pref_header_general=0x7f050013;
+        /**  Example settings for Notifications 
+         */
+        public static final int pref_header_notifications=0x7f05001c;
+        public static final int pref_ringtone_silent=0x7f05001f;
+        public static final int pref_title_add_friends_to_messages=0x7f050018;
+        public static final int pref_title_display_name=0x7f050016;
+        public static final int pref_title_new_message_notifications=0x7f05001d;
+        public static final int pref_title_ringtone=0x7f05001e;
+        public static final int pref_title_social_recommendations=0x7f050014;
+        public static final int pref_title_sync_frequency=0x7f05001a;
+        public static final int pref_title_system_sync_settings=0x7f05001b;
+        public static final int pref_title_vibrate=0x7f050020;
+        /**  Strings related to login 
+         */
+        public static final int prompt_email=0x7f050008;
+        public static final int prompt_password=0x7f050009;
+        public static final int title_activity_login=0x7f050007;
+        public static final int title_activity_settings=0x7f050012;
+        public static final int title_item_detail=0x7f050001;
+        public static final int title_item_list=0x7f050002;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f070000;
+        public static final int LoginFormContainer=0x7f070001;
+    }
+    public static final class xml {
+        public static final int pref_data_sync=0x7f040000;
+        public static final int pref_general=0x7f040001;
+        public static final int pref_headers=0x7f040002;
+        public static final int pref_notification=0x7f040003;
     }
 }
